2b49d1d0b230704c3dfb262d3e74fedacf55626d
Umut <umutdr98@gmail.com>
2020-01-16 19:03:38 +0300

    Entities Layer Created Personel Entity Added

---
+++ TelephoneBook.Entities/Personnel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelephoneBook.Entities
+{
+    public class Personnel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Role { get; set; }
+
+        public string Department { get; set; }
+
+        public string Phone { get; set; }
+
+    }
+}
