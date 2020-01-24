d86cf68be0ac25e6045ff2e89d9e2a20cdaa09ae
Umut <umutdr98@gmail.com>
2020-01-24 11:48:50 +0300

    Admin Area Created

--- TelephoneBook.UI/App_Start/RouteConfig.cs
+++ TelephoneBook.UI/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace TelephoneBook.UI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new string[] { "TelephoneBook.UI.Controllers" } //Admindeki HomeController ile çakışmaması için
             );
         }
     }
